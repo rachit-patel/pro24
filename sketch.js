1dc56615a7f7212ff37b28d75c2e221258210ac0
rachit-patel <74806518+rachit-patel@users.noreply.github.com>
2021-02-16 15:00:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var stone,rubber,hammer;
+
+function setup() {
+	var canvas = createCanvas(1200, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(600,height,1200,20)
+	stone = new Stone(700,320,100,100);
+	rubber=new Rubber(900,450,70);
+    hammer = new Hammer(10,100);
+
+	
+}
+
+
+function draw() {
+  background("lightBlue");
+  Engine.update(engine);
+
+   stone.display();
+   ground.display();
+   rubber.display();
+
+   hammer.display();
+}
+
+
+
